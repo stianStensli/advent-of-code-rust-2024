c003b1798a0c0b224de9299054d80e1aaa957913
stianStensli <stian.hs@hotmail.com>
2024-12-02 09:05:52 +0100

    solve day 2: rust go brrr

--- README.md
+++ README.md
@@ -17,9 +17,10 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `42.4µs` | `54.7µs` |
+| [Day 1](./src/bin/01.rs) | `40.9µs` | `56.0µs` |
+| [Day 2](./src/bin/02.rs) | `47.2µs` | `223.9µs` |
 
-**Total: 0.10ms**
+**Total: 0.37ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,11 +1,144 @@
 advent_of_code::solution!(2);
 
-pub fn part_one(_: &str) -> Option<u64> {
-    None
+pub fn part_one(input: &str) -> Option<i64> {
+    Some(input.lines().map(|line| {
+        let mut last: u32 = 0;
+        let mut init = true;
+        let mut result = 1;
+        let mut dir = 0;
+        line.split_whitespace().for_each(|value| {
+            if result == 0 {
+                return;
+            };
+            if init {
+                init = false;
+                last = value.parse().unwrap();
+            } else {
+                let next: u32 = value.parse().unwrap();
+                if next == last {
+                    result = 0;
+                    return;
+                }
+                if next.abs_diff(last) < 4 {
+                    let new_dir = if next > last  { 2 } else { 1 };
+                    if dir == 0 {
+                        dir = new_dir;
+                        last = next;
+                        return;
+                    }
+                    if new_dir == dir {
+                        last = next;
+                        return;
+                    }
+                }
+                result = 0;
+                last = next;
+                return;
+            }
+        });
+        result
+    }).sum())
 }
 
-pub fn part_two(_: &str) -> Option<u64> {
-    None
+pub fn is_ok2(line: Vec<u32>) -> bool {
+    let mut last: u32 = 0;
+    let mut init = true;
+    let mut result = 1;
+    let mut dir = 0;
+    line.iter().for_each(|next| {
+        let next = *next;
+        if result == 0 {
+            return;
+        };
+        if init {
+            init = false;
+            last = next
+        } else {
+            if next == last {
+                result = 0;
+                return;
+            }
+            if next.abs_diff(last) < 4 {
+                let new_dir = if next > last  { 2 } else { 1 };
+                if dir == 0 {
+                    dir = new_dir;
+                    last = next;
+                    return;
+                }
+                if new_dir == dir {
+                    last = next;
+                    return;
+                }
+            }
+            result = 0;
+            last = next;
+            return;
+        }
+    });
+    result == 1
+}
+
+pub fn is_ok(line: Vec<&u32>) -> bool {
+    let mut last: u32 = 0;
+    let mut init = true;
+    let mut result = 1;
+    let mut dir = 0;
+    line.iter().for_each(|next| {
+        let next = **next;
+        if result == 0 {
+            return;
+        };
+        if init {
+            init = false;
+            last = next
+        } else {
+            if next == last {
+                result = 0;
+                return;
+            }
+            if next.abs_diff(last) < 4 {
+                let new_dir = if next > last  { 2 } else { 1 };
+                if dir == 0 {
+                    dir = new_dir;
+                    last = next;
+                    return;
+                }
+                if new_dir == dir {
+                    last = next;
+                    return;
+                }
+            }
+            result = 0;
+            last = next;
+            return;
+        }
+    });
+    result == 1
+}
+
+
+pub fn part_two(input: &str) -> Option<i64> {
+    Some(input.lines().map(|line| {
+        let values: Vec<u32> = line.split_whitespace()
+            .map(|l| l.parse().unwrap()).collect();
+        let temp2 = values.clone();
+        let temp:Vec<&u32> = temp2.iter().collect();
+        if is_ok(temp) {
+            return 1
+        }
+        for skip in 0..values.len() {
+            let temp3  =  values.clone();
+            let temp2 = temp3.into_iter();
+            let temp: Vec<u32> = temp2.enumerate()
+                .filter(|&(i, _)| i != skip)
+                .map(|(_, v)| v)
+                .collect::<Vec<_>>();
+            if is_ok2(temp) {
+                return 1
+            }
+        }
+        0
+    }).sum())
 }
 
 #[cfg(test)]
@@ -15,12 +148,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
